fb880fef2b4f988fc1525c6f8f42ff3d9c3c3ef9
Tomislav Simovic <tomislav.simovic@yahoo.com>
2024-09-23 18:17:30 +0200

    adding "/" to path parameter

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from . import views
 
 # Create your models here.
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def shop(request):
-    return render(request, 'shop.html')
+    return render(request, 'shop/shop.html')
 
 def warenkorb(request):
-    return render(request, 'warenkorb.html')
+    return render(request, 'shop/warenkorb.html')
 
 def kasse(request):
-    return render(request, 'kasse.html')
+    return render(request, 'shop/kasse.html')
