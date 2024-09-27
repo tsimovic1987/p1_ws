b517d164ac6829a0d55085346144a53c13c2f279
Tomislav Simovic <116288908+tsimovic1987@users.noreply.github.com>
2024-09-27 17:40:17 +0200

    adding the bootstrap css libary

--- api/models.py
+++ api/models.py
@@ -4,3 +4,6 @@ class Notebook(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
