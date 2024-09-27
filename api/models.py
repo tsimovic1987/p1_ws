b6127a1a0e8f174ea4dcaf71d19c0216d66d7852
Tomislav Simovic <116288908+tsimovic1987@users.noreply.github.com>
2024-09-27 17:42:40 +0200

    creating a api app with django

--- api/models.py
+++ api/models.py
@@ -6,4 +6,4 @@ class Notebook(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return '%s %s' % (self.title, self.body)
