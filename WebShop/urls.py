175850ae8deff8eb3feaca8220b7333a2f96e75a
Tomislav Simovic <116288908+tsimovic1987@users.noreply.github.com>
2024-09-28 15:39:34 +0200

    creating a api app with django

--- WebShop/urls.py
+++ WebShop/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from api.resources import NoteResource
 
+note_resource = NoteResource()
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
+    re_path(r'^api/', include(note_resource.urls)),
 ]
